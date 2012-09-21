371ccc8d2f6cd467f9a68a69609202bd8ac1a38b
Christopher Cole <cole14@purdue.edu>
2012-09-21 16:53:32 -0400

    Board.isStinky had x and y swapped -- fixed it.

--- src/environment/Board.java
+++ src/environment/Board.java
@@ -51,8 +51,8 @@ public class Board {
 
     //Query the stinkyness of a tile
     public boolean isStinky(int x, int y){
-        for(int i = x-1; i <= x+1; i++)
-            for(int j = y-1; j <= y+1; j++)
+        for(int i = y-1; i <= y+1; i++)
+            for(int j = x-1; j <= x+1; j++)
                 if(isValid(j, i) && tiles[i][j].hasWumpus)
                     return true;
         return false;
